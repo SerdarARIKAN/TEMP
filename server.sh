9e7fedb6c1e4b06440f630a45b229bac8b6e4c6e
SerdarARIKAN <SerdarARIKAN@gmail.com>
2018-08-15 15:14:52 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,3 +1,5 @@
+##################################################
+
 # sudo su root
 # passwd
 
@@ -6,6 +8,8 @@
 # sed -i "s#ChallengeResponseAuthentication no#ChallengeResponseAuthentication yes#g" "/etc/ssh/sshd_config"
 
 # /etc/init.d/ssh reload
+
+##################################################
 
 # lsblk
 # mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
@@ -18,24 +22,67 @@ echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/sd/b ext4 discard,defaults
 # cat /etc/fstab
 # cd /mnt/sd/b
 
+##################################################
+
 apt-get update
 apt-get upgrade
 apt-get install python3-pip libapache2-mod-wsgi-py3 curl wget apt-transport-https dirmngr dnsutils default-libmysqlclient-dev git
+
+##################################################
 
 wget http://software.virtualmin.com/gpl/scripts/install.sh; /bin/sh install.sh
 
 a2enmod wsgi
 systemctl restart apache2
 
+##################################################
+
 pip3 install virtualenv
+
+##################################################
+
+/etc/mysql/mariadb.conf.d/50-server.cnf
+[mysqld]
+
+# bind-address    = 127.0.0.1
+datadir   = /var/sd/b/mysql
+innodb_read_io_threads = 4
+innodb_write_io_threads = 4
+connect_timeout = 31536000
+interactive_timeout = 31536000
+innodb_data_file_path = ibdata1:3221225472:autoextend
+tmp_table_size = 18446744073709551615
+max_heap_table_size = 18446744073709551615
+innodb_buffer_pool_size = 4294967296
+
+##################################################
+
+SHOW VARIABLES like 'innodb_read_io_threads';
+SHOW VARIABLES like 'innodb_write_io_threads';
+SHOW VARIABLES like 'connect_timeout';
+SHOW VARIABLES like 'interactive_timeout';
+SHOW VARIABLES like 'innodb_data_file_path';
+SHOW VARIABLES like 'tmp_table_size';
+SHOW VARIABLES like 'max_heap_table_size';
+SHOW VARIABLES like 'innodb_buffer_pool_size';
+SHOW STATUS
+
+##################################################
+
+mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '********' WITH GRANT OPTION;"
+
+##################################################
 
 mkdir /mnt/sd/b/mysql/
 rsync -av /var/lib/mysql /mnt/sd/b
 
-# (/etc/mysql/mariadb.conf.d/50-server.cnf)
-# datadir   = /var/lib/mysql >>> datadir    = /mnt/sd/mysql 
-# bind-address    = 127.0.0.1 >>>
-
-mysql -u root -p -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '********' WITH GRANT OPTION;"
+##################################################
 
 systemctl restart mysql
+
+##################################################
+
+nohup mysql --user=root --database=cautolog --password=******** < /mnt/sd/b/tecdoc2017q2/tecdoc.sql &
+ps aux
+
+##################################################
