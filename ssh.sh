3dcd76dc2096eb86b94d31a8d9ba2fd422cc152a
SerdarARIKAN <SerdarARIKAN@gmail.com>
2019-02-03 19:18:13 +0300

    Update and rename !ssh.sh to ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,8 +1,16 @@
+##################################################
+
 sudo su root
 passwd
+
+##################################################
 
 sed -i "s#PermitRootLogin no#PermitRootLogin yes#g" "/etc/ssh/sshd_config"
 sed -i "s#PasswordAuthentication no#PasswordAuthentication yes#g" "/etc/ssh/sshd_config"
 sed -i "s#ChallengeResponseAuthentication no#ChallengeResponseAuthentication yes#g" "/etc/ssh/sshd_config"
 
+##################################################
+
 /etc/init.d/ssh reload
+
+##################################################
