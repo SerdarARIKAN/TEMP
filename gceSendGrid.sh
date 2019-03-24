548737d1c95ca628cd036ae3af9e41281cb2aa4a
SerdarARIKAN <SerdarARIKAN@gmail.com>
2019-03-24 21:09:45 +0300

    Update gceSendGrid.sh

--- gceSendGrid.sh
+++ gceSendGrid.sh
@@ -1,12 +1,15 @@
 ##################################################
 
 # nano /etc/postfix/main.cf
-relayhost = [smtp.sendgrid.net]:2525
-smtp_tls_security_level = encrypt
+relayhost = smtp.sendgrid.net:2525
+smtp_tls_security_level = dane
 smtp_sasl_auth_enable = yes
 smtp_sasl_password_maps = hash:/etc/postfix/sasl_passwd
 header_size_limit = 4096000
 smtp_sasl_security_options = noanonymous
+message_size_limit = 1024000000
+smtpd_tls_CAfile = /etc/postfix/postfix.ca.pem
+smtpd_tls_security_level = may
 
 ##################################################
 
@@ -17,5 +20,17 @@ rm /etc/postfix/sasl_passwd
 chmod 600 /etc/postfix/sasl_passwd.db
 ls -la /etc/postfix/sasl_passwd.db
 /etc/init.d/postfix restart
+
+##################################################
+
+# /etc/php/7.0/apache2/php.ini
+upload_max_filesize = 1024M
+post_max_size = 1024M
+
+##################################################
+
+# /home/user-name/etc/php7.0/php.ini
+upload_max_filesize = 1024M
+post_max_size = 1024M
 
 ##################################################
